3821f78226d1b8b40fd0cff6e76a53379c4b4054
Jose Lezama <joseglego@gmail.com>
2019-07-28 14:34:22 -0400

    chore(init.el): remove dumb enter in a line

--- init.el
+++ init.el
@@ -72,8 +72,7 @@
 ;; Saves emacs backups in other folder
 (setq
  backup-by-copying t              ; don't clobber symlinks
- backup-directory-alist
- '(("." . "~/.emacs_backups"))    ; don't litter my fs tree
+ backup-directory-alist '(("." . "~/.emacs_backups"))    ; don't litter my fs tree
  delete-old-versions t
  kept-new-versions 6
  kept-old-versions 2
