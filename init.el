2e7045ad85f4e18f008de675ad4568ee12224687
Jose Lezama <joseglego@gmail.com>
2019-07-28 16:03:37 -0400

    feat(core): include isolated defs of core

--- init.el
+++ init.el
@@ -7,6 +7,8 @@
 ;;
 ;;; Code:
 
+(package-initialize)
+
 (setq lego-packages (expand-file-name "lego-packages.el" user-emacs-directory))
 (load lego-packages)
 
@@ -16,44 +18,9 @@
 (setq lego-keybinding (expand-file-name "lego-keybinding.el" user-emacs-directory))
 (load lego-keybinding)
 
-(package-initialize)
+(setq lego-core (expand-file-name "lego-core.el" user-emacs-directory))
+(load lego-core)
 
-;; Basic: User
-(setq user-full-name "Jose Lezama")
-(setq user-mail-address "me@joseglego.io")
-
-;; Basic: WhiteSpace Cleanup
-(global-whitespace-cleanup-mode)
-
-;; Basic: Use spaces instead spaces
-(setq-default indent-tabs-mode nil)
-
-;; Basic: Maximize Buffer
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
-;; C-c <left> to go back
-
-;; Saves emacs backups in other folder
-(setq
- backup-by-copying t              ; don't clobber symlinks
- backup-directory-alist '(("." . "~/.emacs_backups"))    ; don't litter my fs tree
- delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t)               ; use versioned backups
-
-;; Basic: Auto revert mode (Updating buffers when changed on disk)
-(global-auto-revert-mode)
-
-;; Move Backup files.
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; Execute shell
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
 ;; Define Major mode for Web Dev
 (require 'web-mode)
 ;; HTML/SCSS
---
+++ lego-core.el
@@ -0,0 +1,38 @@
+;; Basic: User
+(setq user-full-name "Jose Lezama")
+(setq user-mail-address "me@joseglego.io")
+
+;; Basic: WhiteSpace Cleanup
+(global-whitespace-cleanup-mode)
+
+;; Basic: Use spaces instead spaces
+(setq-default indent-tabs-mode nil)
+
+;; Basic: Maximize Buffer
+(when (fboundp 'winner-mode)
+  (winner-mode 1))
+;; C-c <left> to go back
+
+;; Saves emacs backups in other folder
+(setq
+ backup-by-copying t              ; don't clobber symlinks
+ backup-directory-alist '(("." . "~/.emacs_backups"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)               ; use versioned backups
+
+;; Basic: Auto revert mode (Updating buffers when changed on disk)
+(global-auto-revert-mode)
+
+;; Move Backup files.
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Execute shell
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(provide 'lego-core)
