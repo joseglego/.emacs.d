6222c0497f62c5e3464eee38b5b399c613c74664
Jose Lezama <joseglego@gmail.com>
2019-07-28 16:39:13 -0400

    feat(show-hide): define isolated show-hide setup

--- init.el
+++ init.el
@@ -142,35 +142,6 @@
 (add-to-list 'ac-modes 'angular-mode)
 (add-to-list 'ac-modes 'angular-html-mode)
 
-;; Activate Show&Hide Minor Mode!
-(defun toggle-selective-display (column)
-      (interactive "P")
-      (set-selective-display
-       (or column
-           (unless selective-display
-             (1+ (current-column))))))
-
-(defun toggle-hiding (column)
-       (interactive "P")
-       (if hs-minor-mode
-         (if (condition-case nil
-                              (hs-toggle-hiding)
-                            (error t))
-                          (hs-show-all))
-                    (toggle-selective-display column)))
-(load-library "hideshow")
-  (global-set-key (kbd "C-+") 'toggle-hiding)
-  (global-set-key (kbd "C-\\") 'toggle-selective-display)
-
-  (add-hook 'c-mode-common-hook   'hs-minor-mode)
-  (add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
-  (add-hook 'java-mode-hook       'hs-minor-mode)
-  (add-hook 'lisp-mode-hook       'hs-minor-mode)
-  (add-hook 'perl-mode-hook       'hs-minor-mode)
-  (add-hook 'sh-mode-hook         'hs-minor-mode)
-  (add-hook 'js-mode-hook         'hs-minor-mode)
-  (add-hook 'web-mode-hook        'hs-minor-mode)
-
 ;;
 (require 'rbenv)
 (global-rbenv-mode)
@@ -222,3 +193,4 @@
 
 ;; Require core config
 (require 'setup-flycheck)
+(require 'setup-show-hide)
