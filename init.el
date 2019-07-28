e84164752f159fb343e50ecaafc4bae97c74c518
Jose Lezama <joseglego@gmail.com>
2019-07-28 16:41:49 -0400

    feat(powerline): define isolated powerline setup

--- init.el
+++ init.el
@@ -97,21 +97,6 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
-;; Powerline
-(require 'powerline)
-(powerline-default-theme)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:foreground "#030303" :background "#6d0204" :box nil))))
- '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
- '(mode-line-inactive ((t (:foreground "#ffffff" :background "#5d6365" :box nil))))
- '(powerline-active1 ((t (:foreground "#f9f9f9" :background "#ff6365" :box nil))))
- '(powerline-active2 ((t (:foreground "#f9f9f9" :background "#5d6365" :box nil))))
- '(powerline-arrow-shape (quote arrow)))
-
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
@@ -194,3 +179,4 @@
 ;; Require core config
 (require 'setup-flycheck)
 (require 'setup-show-hide)
+(require 'setup-powerline)
