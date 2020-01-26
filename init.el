9944c334c6f977a71498a74d987974acf0a1a2b6
José Lezama <joseglego@gmail.com>
2020-01-26 15:17:02 -0300

    feat(rgrep): define config

--- init.el
+++ init.el
@@ -19,6 +19,11 @@
 (add-to-list 'load-path core-dir)
 (add-to-list 'load-path setup-dir)
 
+(eval-after-load "grep"
+  '(progn
+    (add-to-list 'grep-find-ignored-files "*.tmp")
+    (add-to-list 'grep-find-ignored-directories "node_modules")))
+
 ;; Require core config
 (require 'lego-packages)
 (require 'lego-ui)
